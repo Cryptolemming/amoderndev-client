ce7f16591540199956c2f8d43c839fcf7490d1f2
cryptolemming <aliayoub22@gmail.com>
2020-02-04 13:04:19 -0500

    add: basic featured post scrolling

--- src/components/home/FeaturedList.js
+++ src/components/home/FeaturedList.js
@@ -4,25 +4,37 @@ import FeaturedItem from './FeaturedItem';
 
 export default class FeaturedList extends Component {
 
-  render() {
+  constructor(props) {
+    super(props)
+    this.featuredList = React.createRef()
+  }
 
+  onArrowClick = (direction) => {
+    if (direction === 'l') {
+      this.featuredList.current.scrollLeft += 150;
+    } else {
+      this.featuredList.current.scrollLeft -= 150;
+    }
+  }
+
+  render() {
     const list = this.generateList();
 
     return (
       <section className='featured-list-container'>
         <h3 className='home-list-title'>Featured</h3>
         <section className='horizontal-container'>
-          <span className='arrow'>
+          <span className='arrow' onClick={() => this.onArrowClick('l')}>
             &lArr;
           </span>
           <div className='fadeout-featured ff-left'>
           </div>
-          <ul className='featured-list'>
+          <ul className='featured-list' ref={this.featuredList}>
             {list}
           </ul>
           <div className='fadeout-featured ff-right'>
           </div>
-          <span className='arrow'>
+          <span className='arrow' onClick={() => this.onArrowClick('r')}>
             &rArr;
           </span>
         </section>
