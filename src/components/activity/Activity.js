5d5a8a751859087fa6b85f5d58129cf840a5dd2f
cryptolemming <aliayoub22@gmail.com>
2020-02-14 09:17:28 -0500

    add: start activity component

--- src/components/activity/Activity.js
+++ src/components/activity/Activity.js
@@ -5,9 +5,19 @@ export default class Activity extends Component {
 
   render() {
 
+    const { user } = this.props;
+
+    const navJSX = this.generateNavJSX();
+    const infoJSX = this.generateInfoJSX(user);
+
     return (
       <section className='activity'>
-        Activity
+        <section className='activity-nav'>
+          {navJSX}
+        </section>
+        <section className='activity-info'>
+          {infoJSX}
+        </section>
       </section>
     )
   }
