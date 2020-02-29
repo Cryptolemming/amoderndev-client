8961f292a915046ddc54fe09549ad16b04753159
cryptolemming <aliayoub22@gmail.com>
2020-02-28 20:04:51 -0500

    add: activity post favourite functionality

--- src/components/activity/ActivityPostItem.js
+++ src/components/activity/ActivityPostItem.js
@@ -3,76 +3,126 @@ import './ActivityPostItem.css';
 import { postControlIcons } from '../../constants';
 import uuid from 'uuid/v4';
 import { getTimePassed } from '../../helpers';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { addFavourite, deleteFavourite, deletePost } from '../../actions';
 
-export default ({id, user, date_created, title, content, controls, mods,
-                handleDelete, comments, favouritesUsers, commentsUsers}) => {
+export class PostItem extends Component {
 
-  const date = getTimePassed(date_created)
-  const counts = {
-    'c': comments.length,
-    'f': favouritesUsers.length
+  handleClickFavourite = () => {
+    console.log('click favourite')
+    const { id: postId, user, dispatch, history, posts } = this.props;
+    const favouritesUsers = posts ? posts[postId].favouritesUsers : [];
+    // if there's a user
+    if (user) {
+      favouritesUsers.includes(user.id)
+        ? dispatch(deleteFavourite('post', parseInt(postId)))
+        : dispatch(addFavourite('post', parseInt(postId)))
+    }
+    else {
+      history.push('/authenticate/login')
+    }
   }
 
-  const activeClass = {
-    'f': user && favouritesUsers.includes(user.id) ? 'active' : 'inactive',
-    'c': user && commentsUsers.includes(user.id) ? 'active' : 'inactive'
+  render() {
+    const date = getTimePassed(date_created)
+    const { id, user, date_created, title, content, controls, mods,
+            handleDelete, comments, favouritesUsers, commentsUsers } = this.props;
+
+    const controlsJSX = this.generateControlsJSX(controls);
+    const modsJSX = this.generateModsJSX(mods);
+
+      return (
+          <li className='activity-post-item'>
+            <div className='activity-post-text'>
+                <Link to={`/posts/${id}`}>
+                  <p className='activity-post-title'>
+                    {title}
+                  </p>
+                  <p className='activity-post-date'>
+                    {date}
+                  </p>
+                </Link>
+            </div>
+            <div className='activity-post-controls'>
+              {controlsJSX}
+              {modsJSX}
+            </div>
+          </li>
+      )
   }
 
-  const controlsJSX = controls.map(control => {
-    return <span
-      key={uuid()}
-      className='activity-control-item'>
-      <span className={`post-controls-text-item control-${activeClass[control]}`}>
-        <span className='control-count'>{counts[control]}</span>
-        {postControlIcons[control]}
-      </span>
-    </span>
-  })
+  generateModsJSX = (mods, id) => {
+    const modsJSX = mods.map(mod => {
+      const actions = {
+        'd': {
+          onClick: () => this.props.dispatch(deletePost(id))
+        },
+        'e': {}
+      }
 
-  const modsJSX = mods.map(mod => {
-    const actions = {
-      'd': {
-        onClick: () => handleDelete(id)
-      },
-      'e': {}
+      const content = mod === 'd'
+        ? postControlIcons[mod]
+        : <Link
+            key={uuid()}
+            to='/edit-post'
+          >
+            {postControlIcons[mod]}
+          </Link>
+
+      const span = <span
+                    key={uuid()}
+                    className='activity-control-item'
+                    {...actions[mod]}>
+                    {content}
+                  </span>
+
+      return span;
+    })
+
+    return modsJSX;
+  }
+
+  generateControlsJSX = () => {
+    const { comments, favouritesUsers, commentsUsers, user, controls } = this.props;
+
+    const navMap = {
+      'kp': () => {},
+      'f': this.handleClickFavourite,
+      'b': () => {},
+      'c': () => {}
     }
 
-    const content = mod === 'd'
-      ? postControlIcons[mod]
-      : <Link
-          key={uuid()}
-          to='/edit-post'
-        >
-          {postControlIcons[mod]}
-        </Link>
+    const counts = {
+      'c': comments.length,
+      'f': favouritesUsers.length
+    }
 
-    const span = <span
-                  key={uuid()}
-                  className='activity-control-item'
-                  {...actions[mod]}>
-                  {content}
-                </span>
+    const activeClass = {
+      'f': user && favouritesUsers.includes(user.id) ? 'active' : 'inactive',
+      'c': user && commentsUsers.includes(user.id) ? 'active' : 'inactive'
+    }
 
-    return span;
-  })
+    const controlsJSX = controls.map(control => {
+      return <span
+        key={uuid()}
+        onClick={navMap[control]}
+        className='activity-control-item'>
+        <span className={`post-controls-text-item control-${activeClass[control]}`}>
+          <span className='control-count'>{counts[control]}</span>
+          {postControlIcons[control]}
+        </span>
+      </span>
+    })
 
-  return (
-      <li className='activity-post-item'>
-        <div className='activity-post-text'>
-            <Link to={`/posts/${id}`}>
-              <p className='activity-post-title'>
-                {title}
-              </p>
-              <p className='activity-post-date'>
-                {date}
-              </p>
-            </Link>
-        </div>
-        <div className='activity-post-controls'>
-          {controlsJSX}
-          {modsJSX}
-        </div>
-      </li>
-  )
+    return controlsJSX;
+  }
+
 }
+
+const mapStateToProps = state => ({
+  user: state.user,
+  posts: state.posts
+})
+
+export default connect(mapStateToProps)(withRouter(PostItem))
